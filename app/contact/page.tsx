67162b7cf02f28039c344187431b0b7e2b556d35
prog <email@gmail.com>
2023-07-02 16:23:58 +0700

    Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page: React.FC = () => {
+  console.log('Contact');
+  return (
+      <>
+        <h1 className='text-white mt-20'>Contact Page</h1>
+      </>
+  );
+}
+
+export default Page;
