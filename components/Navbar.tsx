4da5542d5a311056e3ac06dc3b4910dc0d3cace7
prog <email@gmail.com>
2023-07-02 15:51:56 +0700

    Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { ProfileIcon } from "@/components/ProfileIcon";
+import React from "react";
+
+export const Navbar: React.FC = () => {
+  return (
+      <nav className='flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-blue-800'>
+        <Link href={'/'}><a className='font-bold'>Product site name</a></Link>
+        <ProfileIcon/>
+      </nav>
+  );
+};
