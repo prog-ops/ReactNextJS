7eab6db7d9ba70c872dbc830c5912b5d7889d6ba
prog <email@gmail.com>
2023-07-02 16:12:45 +0700

    Next auth provider

---
+++ app/provider/provider.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { SessionProvider } from "next-auth/react";
+import React, { ReactNode } from "react";
+
+interface NextAuthProviderProps {
+  children: ReactNode;
+}
+
+const NextAuthProvider: React.FC<NextAuthProviderProps> = ({ children }) => {
+  return (
+      <SessionProvider>
+        {children}
+      </SessionProvider>
+  );
+}
+
+export default NextAuthProvider;
